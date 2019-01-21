32641f7db3fcd3a0864cd73027e940a6797901bc
Nikunj Kanetiya <kanetiya@gmail.com>
2019-01-21 14:24:05 +0530

    Add toggle nav event

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -12,13 +12,16 @@
         <svg
           class="fill-current h-3 w-3"
           viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg">
+          xmlns="http://www.w3.org/2000/svg"
+          @click="toggle">
           <title>Menu</title>
           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
         </svg>
       </button>
     </div>
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div
+      :class="isOpen ? 'block': 'hidden'"
+      class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
       <div class="text-sm lg:flex-grow">
         <nuxt-link
           to="/"
@@ -38,7 +41,15 @@
 
 <script>
 export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  },
   methods: {
+    toggle() {
+      this.isOpen = !this.isOpen
+    },
     doLogout() {
       this.$auth.logout().then(res => {
         this.$router.replace({ path: '/auth/login' })
