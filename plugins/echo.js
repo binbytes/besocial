385991a05e6a4eacdd8004aebcdfaf516fe12117
Nikunj Kanetiya <kanetiya@gmail.com>
2019-01-26 13:06:18 +0530

    WIP - Update echo listening

--- plugins/echo.js
+++ plugins/echo.js
@@ -1,8 +1,9 @@
 import Echo from 'laravel-echo'
 import Pusher from 'pusher-js'
+import Vue from 'vue'
 
-export default ({ app }, inject) => {
-  app.$echo = new Echo({
+export default () => {
+  Vue.prototype.$echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.PUSHER_KEY,
     wsHost: process.env.WEBSOCKET_HOST,
